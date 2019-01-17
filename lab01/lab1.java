323962642467737f6572cbc30dca55f0ed6914ab
darthkaran <karandams@gmail.com>
2019-01-17 09:06:11 -0500

    Karan adding to Spencers REPO

--- lab01/lab1.java
+++ lab01/lab1.java
@@ -1,8 +1,9 @@
-class lab1 
-{ 
-    
-    public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
-    } 
-} 
+class lab1
+{
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+        System.out.println("Hello, Spence - Karan"); 
+    }
+}
